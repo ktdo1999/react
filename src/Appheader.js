9b3fade234ff6fe64fb1ee3ea16ba37168de036a
ktdo1999 <78062594+ktdo1999@users.noreply.github.com>
2021-02-17 12:20:03 +0700

    Add files via upload

---
+++ src/Appheader.js
@@ -0,0 +1,10 @@
+function Appheader(){
+return (
+    //<div className="App">
+      <header className="App-header">
+          <img className ="header-logo" src ="/pic/1.jpg"/>
+      </header>
+  //  </div>
+  );
+}
+export default Appheader;
---
+++ src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/pic/2.jpg"
+      
+    
+    },
+    {
+      title :"gril",
+      thumbnaiUrl :"/pic/3.jpg"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/pic/4.jpg"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/5.jpg"
+      
+    
+    }
+  ];
+  export default items;
